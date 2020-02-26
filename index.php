3a09d8c94f2d828588c43c05230b901ca223a464
RMG8 <56052470+RahelMaier@users.noreply.github.com>
2020-02-25 23:50:34 -0800

    Assignment 7 W20  Functions
    A palindrome is a word hat reads the same backward as forward, e.g., radar or kayak
    Write a  isPalimndrome function that checks whether string, passed as an argument is a palindrome. The function should return true if or false.
    Test your function with a couple differnt words

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
-Assignment 7 W20  Functions
-Write a sortArray function to sort an array. You may not call the built in functions to do the sorting. Your function should take an array as argument and return a sorted array.
-Test that your function is sorted by using print_r of the returned array.
-
 <!DOCTYPE html>
 <html>
 <body>
+<?php 
 
-<?php
+    $str = 'radar';
+    $strLen = strlen($str)-1;
+    $revStr = '';
+    for($i=$strLen; $i>=0; $i--){
+        $revStr.=$str[$i];
+    }
+    if($revStr == $str)
+        echo 'Palindrome';
+    else
+        echo "Not Palindrome";
+      echo "String: " . $word . "<br>";  
+          $str = 'cat';
+    $strLen = strlen($str)-1;
+    $revStr = '';
+    for($i=$strLen; $i>=0; $i--){
+        $revStr.=$str[$i];
+    }
+    if($revStr == $str)
+        echo 'Palindrome';
+    else
+        echo "Not Palindrome";
 
-
-function arraysSum(array ...$arrays): array
-{
-    return array_map(function(array $array): int {
-        return array_sum($array);
-    }, $arrays);
-}
-
-print_r(arraysSum([0,0,-2], [0,0,4], [5,5,5],[5,8,9]));
+echo "String: " . $word . "<br>";
 ?>
 </body>
 </html>
