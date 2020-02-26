6e5a3910befa311a2021da19de76ee9f7e8663e1
RMG8 <56052470+RahelMaier@users.noreply.github.com>
2020-02-25 21:57:40 -0800

    Assignment 7 W20  Functions
    Write a sortArray function to sort an array. You may not call the built in functions to do the sorting. Your function should take an array as argument and return a sorted array.
    Test that your function is sorted by using print_r of the returned array.

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+Assignment 7 W20  Functions
+Write a sortArray function to sort an array. You may not call the built in functions to do the sorting. Your function should take an array as argument and return a sorted array.
+Test that your function is sorted by using print_r of the returned array.
+
 <!DOCTYPE html>
 <html>
 <body>
